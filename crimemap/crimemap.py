50b2c9d8ff30d21e695710cc191c909007802ef3
Tim Gasser <timgasser@hotmail.com>
2017-09-02 23:52:15 -0700

    Added mock of database connection and Javascript with single marker maps

--- crimemap/crimemap.py
+++ crimemap/crimemap.py
@@ -1,10 +1,18 @@
-from dbhelper import DBHelper
+import dbconfig
+if dbconfig.test:
+    from mockdbhelper import MockDBHelper
+else:
+    from dbhelper import DBHelper
+    
 from flask import Flask
 from flask import render_template
 from flask import request
 
 app = Flask(__name__)
-DB = DBHelper()
+if dbconfig.test:
+    DB = MockDBHelper()
+else:
+    DB = DBHelper()
 
 @app.route("/")
 def home():
---
+++ crimemap/mockdbhelper.py
@@ -0,0 +1,16 @@
+import pymysql
+import dbconfig
+
+class MockDBHelper(object):
+
+    def connect(self, database="crimemap"):
+        pass
+
+    def get_all_inputs(self):
+        return []
+
+    def add_input(self, data):
+        pass
+
+    def clear_all(self):
+        pass
